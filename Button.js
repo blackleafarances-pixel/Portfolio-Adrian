b9ab856b9facba4d646dea7344ba2f29c4e9d43b
blackleafarances-pixel <blackleafarances@gmail.com>
2025-12-31 19:11:19 +0800

    Create Button.js

---
+++ Button.js
@@ -0,0 +1,12 @@
+let FaceBookButton = document.getElementById("Facebook_Button")
+let InstagramButton = document.getElementById("Instagram_Button")
+
+FaceBookButton.addEventListener("click", function(){
+    window.open("https://www.facebook.com/adrian.arances", "_blank");
+
+})
+
+InstagramButton.addEventListener("click", function(){
+    window.open("https://www.instagram.com/adrianarancess/", "_blank");
+    
+})
